12f2014edfe2f8c288eae85db273af5e919cdae4
Alícia Stefany <64162281+aliciastefany@users.noreply.github.com>
2026-05-14 18:13:13 +0000

    updace site

---
+++ site-fake-api/src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import "./App.css"
+
+function App(){
+  const [cursos, setCursos] = useState([])
+
+  const url = "https://my-json-server.typicode.com/aliciastefany/fake-api-BG/cursos"
+
+  const carregarCursos = async ()=>{
+    try{
+      const resposta = await fetch(url)
+      const dados = await resposta.json()
+      setCursos(dados)
+    } catch(erro){
+      console.error("Erro ao carregar os dados:", erro)
+    }
+  }
+
+  useEffect(()=>{carregarCursos()}, [])
+
+  return(
+    <div className="container">
+      <button onClick={carregarCursos}>Lista de Cursos</button>
+
+      {cursos.map((curso)=>{
+        <div className="card" key={curso.id}>
+          <h2>{curso.nome}</h2>
+          <p>Carga Horária: {curso["carga-horaria"]} h</p>
+
+          <div className="disciplinas">
+            {curso.disciplinas.map((disc, index)=>{
+              <span className="disciplina" key={index}>{disc}</span>
+            })}
+          </div>
+        </div>
+      })}
+
+    </div>
+  )
+}
+
+export default App
